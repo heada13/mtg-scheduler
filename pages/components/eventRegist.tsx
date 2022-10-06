4755484054e8ce4a3680213f8afbb9ee0a411e9b
heada13 <babolatlist_1003@yahoo.co.jp>
2022-10-06 21:51:19 +0900

    change:chakraUIやめてmaterialUIに変更

--- pages/components/eventRegist.tsx
+++ pages/components/eventRegist.tsx
@@ -1,18 +1,16 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from '../../styles/Home.module.scss'
-import { Select, Input } from '@chakra-ui/react'
 import { Store } from '@prisma/client';
-import { DateTimePicker } from '@mui/x-date-pickers';
-import DateFnsUtils from '@date-io/date-fns'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import TextField from '@mui/material/TextField';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { StaticDateTimePicker } from '@mui/x-date-pickers/StaticDateTimePicker';
 import { MobileDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function EventRegist () {
   const [stores, setStores] = useState<Store[]>([])
+  const [store, setStore] = useState("")
   const [eventName, setEventName ] = useState<string>()
   const [date, setDate] = useState<Date|null>(new Date())
   const getStores =async () => {
@@ -21,7 +19,10 @@ export default function EventRegist () {
     setStores(stores)
   }
   const handleChange = (e:any) => {
-    setEventName(e.target.value)
+    setEventName(e.taxrget.value)
+  }
+  const handleChangeStore = (e:SelectChangeEvent) => {
+    setStore(e.target.value)
   }
   // const changeDateHandler = (date: Date|null):void => {
   //   setDate(date)
@@ -31,27 +32,30 @@ export default function EventRegist () {
   },[])
   return (
     <>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <MobileDateTimePicker
-          value={date}
-          onChange={(newValue) => {
-            setDate(newValue);
-          }}
-          onError={console.log}
-          renderInput={(params) => <TextField {...params} />}
-        />
-      </LocalizationProvider>      
-      <Input 
-      placeholder='イベント名を入力してください' 
-      value={eventName}
-      onChange={handleChange}
-      className={styles.regist_content}>
-      </Input>
-      <Select placeholder='店舗選択'>
-        {stores.map((store) => (
-          <option value={store.id} key={store.id}>{store.store_name}</option>
-          ))}
-      </Select>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <MobileDateTimePicker
+            value={date}
+            onChange={(newValue) => {
+              setDate(newValue);
+            }}
+            onError={console.log}
+            renderInput={(params) => <TextField {...params} />}
+            />
+        </LocalizationProvider>
+        <TextField
+          id="outlined-name"
+          label="Name"
+          value={eventName}
+          onChange={handleChange}
+        />     
+        <Select
+          value={store}
+          // label="Age"
+          onChange={handleChangeStore}>
+          {stores.map((store) => (
+            <MenuItem value={store.id} key={store.id}>{store.store_name}</MenuItem>
+            ))}
+        </Select>
     </>
   )
 }
