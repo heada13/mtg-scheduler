fa9972707a8cf65dfe3fbb557247100059b9f434
heada13 <babolatlist_1003@yahoo.co.jp>
2022-11-03 17:36:46 +0900

    add:next-intercept-stdout追加（しかし警告文消えない）

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withInterceptStdout = require('next-intercept-stdout');
+
+const nextConfig = withInterceptStdout({
   reactStrictMode: false,
   swcMinify: true,
   // pageExtensions: ['.tsx', '.ts', '.jsx', '.js']
-}
+  },
+  (text) => (text.includes('Duplicate atom key') ? '' : text),
+)
 
 module.exports = nextConfig
