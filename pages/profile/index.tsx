e71fc8ab68fba58148753eb8d8009777254ec677
heada13 <babolatlist_1003@yahoo.co.jp>
2022-12-24 16:29:56 +0900

    fix:プロフィール画面見た目調整

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -18,8 +18,15 @@ const Profile = () => {
   return (
     <>
       <div className={styles.profile_container}>
-        <Button onClick={()=> router.push('/editProfile') }>プロフィール変更</Button>
-        { member?.email }
+        {member?.image_file_name === "/" ? 
+        <div className={styles.blank_profile_image}>未設定</div> :
+        <img src={member?.image_file_name} alt="profile-image" className={styles.profile_image}/> 
+        }
+        {member?.name ? 
+        <h2 className={styles.user_name}>{member?.name}</h2> :
+        <h2 className={styles.user_name}>ユーザーネーム未設定</h2>
+        }
+        <Button onClick={()=> router.push('/editProfile') } variant="outlined">プロフィール変更</Button>
       </div>
     </>
   )
