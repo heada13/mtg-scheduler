034ab36f46846ed1ee2792ab7a188b70b2c29a10
heada13 <babolatlist_1003@yahoo.co.jp>
2022-12-31 18:11:09 +0900

    add:カラーテーマの設定追加

---
+++ const/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+const Theme = createTheme({
+  palette: {
+    primary: {
+      main: "#14213D"
+    },
+    secondary: {
+      main: "#37569A"
+    },
+
+  }
+})
+
+export default Theme
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../lib/authContext'
-import { RecoilRoot } from "recoil";
-import { Header } from '../components/header';
+import { RecoilRoot } from "recoil"
+import { Header } from '../components/header'
+import { ThemeProvider } from '@mui/material/styles'
+import Theme from '../const/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <AuthProvider>
-        <Header/>
-        <Component {...pageProps} />
+        <ThemeProvider theme={Theme}>
+          <Header/>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </AuthProvider>
     </RecoilRoot>
   )
